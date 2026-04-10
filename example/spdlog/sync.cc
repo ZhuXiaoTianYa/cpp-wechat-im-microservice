6104c7ea77e708690d9fffccf36e7875aba25363
zhuxiaotianya <1119876462@qq.com>
2026-04-10 23:23:00 +0800

    test: test spdlog library usage

---
+++ example/spdlog/sync.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
+
+int main(int argc, char *argv[])
+{
+    // 设置全局刷新策略
+    spdlog::flush_every(std::chrono::seconds(1));
+    // 设置全局日志输出等级
+    spdlog::flush_on(spdlog::level::level_enum::debug);
+    // 设置全局日志等级
+    spdlog::set_level(spdlog::level::debug);
+
+    // 创建同步日志器
+    // auto logger = spdlog::stdout_color_mt("sync-logger");
+    auto logger = spdlog::basic_logger_mt("file-logger", "log.txt");
+    // 设置日志器刷新策略及输出等级
+    // logger->set_level(spdlog::level::debug);
+    // logger->flush_on(spdlog::level::debug);
+
+    // 设置日志的输出格式
+    logger->set_pattern("[%Y-%m-%d %H:%M:%S][%n][%t][%-8l] %v");
+
+    // 日志输出
+    logger->trace("你好,{}", "烛天");
+    logger->debug("你好,{}", "烛天");
+    logger->info("你好,{}", "烛天");
+    logger->warn("你好,{}", "烛天");
+    logger->error("你好,{}", "烛天");
+    logger->critical("你好,{}", "烛天");
+    std::cout << "日志器输出结束" << std::endl;
+    return 0;
+}
